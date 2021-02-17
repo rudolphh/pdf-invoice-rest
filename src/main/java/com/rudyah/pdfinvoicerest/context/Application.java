ffabab35037b1fff6c7f19298d88ced84a06d1b0
Rudy A. Hernandez <rudolpharthur@gmail.com>
2021-02-16 18:47:33 -0800

    Reorganize, refactor, add user/userServie

--- src/main/java/com/rudyah/pdfinvoicerest/context/Application.java
+++ src/main/java/com/rudyah/pdfinvoicerest/context/Application.java
@@ -2,10 +2,11 @@ package com.rudyah.pdfinvoicerest.context;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rudyah.pdfinvoicerest.service.InvoiceService;
+import com.rudyah.pdfinvoicerest.service.UserService;
 
 public class Application {
 
     public static final InvoiceService invoiceService = new InvoiceService();
     public static final ObjectMapper objectMapper = new ObjectMapper();
-
+    public static final UserService userService = new UserService();
 }
--- src/main/java/com/rudyah/pdfinvoicerest/service/InvoiceService.java
+++ src/main/java/com/rudyah/pdfinvoicerest/service/InvoiceService.java
@@ -1,6 +1,8 @@
 package com.rudyah.pdfinvoicerest.service;
 
 import com.rudyah.pdfinvoicerest.bean.Invoice;
+import com.rudyah.pdfinvoicerest.bean.User;
+import com.rudyah.pdfinvoicerest.context.Application;
 
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
@@ -14,7 +16,10 @@ public class InvoiceService {
     }
 
     public Invoice create(String userId, Integer amount) {
-
+        User user = Application.userService.findById(userId);
+        if (user == null) {
+            throw new IllegalStateException();
+        }
         Invoice invoice = new Invoice(userId, amount, "http://www.africau.edu/images/default/sample.pdf");
         invoices.add(invoice);
         return invoice;
